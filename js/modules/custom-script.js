72e5779ab13fb675b16bd48186c8478d179024ce
MuraliJbk <murali@jbkinfotech.com>
2023-12-24 17:30:09 +0530

    first commit

--- js/modules/custom-script.js
+++ js/modules/custom-script.js
@@ -3,17 +3,31 @@ document.addEventListener("DOMContentLoaded", () =>{
     const header = document.querySelector(".header-main");
     const headerheight = header.clientHeight;
     const banner = document.querySelector(".banner-section");
-    const bannerHeight = banner.offsetTop + banner.clientHeight;
-    window.onscroll = function(){
-        const _scroll = window.scrollY;
-
-        console.log(_scroll, bannerHeight)
-        if (_scroll > bannerHeight) {
-           header.classList.add("fixed-header");
-        }
-        else{
-           header.classList.remove("fixed-header");
-
+    if(banner){
+        const bannerHeight = banner.offsetTop + banner.clientHeight;
+        window.onscroll = function(){
+            const _scroll = window.scrollY;
+    
+            console.log(_scroll, bannerHeight)
+            if (_scroll > bannerHeight) {
+               header.classList.add("fixed-header");
+            }
+            else{
+               header.classList.remove("fixed-header");
+    
+            }
         }
     }
+
+
+    $('.accordion-header').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().toggleClass('active');
+        $(this).parent().siblings().removeClass('active');
+        $(this).parent().siblings().find('.accordion-header').removeClass('open');
+        $(this).toggleClass("open");
+        $(this).siblings('.accordion-content').slideToggle(500);
+        $(this).parent().siblings().find('.accordion-content').slideUp(500);
+    });
+
 });
