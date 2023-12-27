f07dd0c1c27877758a14dbc44eb388722ea96d3d
MuraliJbk <murali@jbkinfotech.com>
2023-12-27 17:08:41 +0530

    first commit

--- js/modules/custom-script.js
+++ js/modules/custom-script.js
@@ -1,23 +1,22 @@
 
-document.addEventListener("DOMContentLoaded", () =>{
-    const header = document.querySelector(".header-main");
-    const headerheight = header.clientHeight;
-    const banner = document.querySelector(".banner-section");
-    if(banner){
-        const bannerHeight = banner.offsetTop + banner.clientHeight;
-        window.onscroll = function(){
-            const _scroll = window.scrollY;
+jQuery(document).ready(function($){
+    // const header = $(".header-main");
+    // const headerheight = header.clientHeight;
+    // const banner = $(".banner-section");
+    // if(banner){
+    //     const bannerHeight = banner.offsetTop + banner.clientHeight;
+    //     window.onscroll = function(){
+    //         const _scroll = window.scrollY;
+    //         console.log(_scroll, bannerHeight)
+    //         if (_scroll > bannerHeight) {
+    //            header.classList.add("fixed-header");
+    //         }
+    //         else{
+    //            header.classList.remove("fixed-header");
     
-            console.log(_scroll, bannerHeight)
-            if (_scroll > bannerHeight) {
-               header.classList.add("fixed-header");
-            }
-            else{
-               header.classList.remove("fixed-header");
-    
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
 
     $('.accordion-header').on('click', function(e){
         e.preventDefault();
@@ -28,5 +27,20 @@ document.addEventListener("DOMContentLoaded", () =>{
         $(this).siblings('.accordion-content').slideToggle(500);
         $(this).parent().siblings().find('.accordion-content').slideUp(500);
     });
+
+    const tablink = $("ul.about-partners-tabs li a");
+    const tablinkFirst = $("ul.about-partners-tabs li:first-child a");
+    tablinkFirst.addClass("active");
+    tabValue = $(".partners-tab-content").attr("data-tab-value");
+
+    tablink.on("click", function(e){
+        e.preventDefault();
+        $(this).addClass("active");
+        $(this).parent().siblings().find("a").removeClass("active");
+        const tabAttr = $(this).attr("data-tab-name");
+        $(".partners-tab-content").hide();
+        $(".partners-tab-content[data-tab-value="+ tabAttr +"]").fadeIn();
+    });
+
 
 });
