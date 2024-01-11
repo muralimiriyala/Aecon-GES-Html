009d80dc3c24ce9ccec1045648cd03f598655194
MuraliJbk <murali@jbkinfotech.com>
2024-01-11 14:25:30 +0530

    deve.

--- js/modules/custom-slick.js
+++ js/modules/custom-slick.js
@@ -4,7 +4,7 @@ import jQuery from 'jquery';
 import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
 
-jQuery(document).ready(function($){
+jQuery(function($){
     const partnerLogoSlider = $(".partner-logos-slider");
     partnerLogoSlider.slick({
         slidesToShow: 5,
@@ -77,49 +77,87 @@ jQuery(document).ready(function($){
       });
       $(".timeline-slide").removeAttr("style");
 
-      const psliderFor = $(".process-slider");
-      psliderFor.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        cssEase: 'ease',
-        arrows: false,
-        dots: true,
-        appendDots: $("<div class='slick-dots-wrap'></div>"),
-      });
+    
+      const psliderFor = $(".process-slider-for");
+      const psliderNav = $(".process-slider-nav");
 
-      // Set initial class for the first dot
-      $('.p-slick-dot[data-slide="0"]').addClass('slick-current slick-first-slide');
-      psliderFor.on('afterChange', function (event, slick, currentSlide, nextSlide) {
-        $('.p-slick-dot').removeClass('slick-current');
-        $('.p-slick-dot[data-slide="' + currentSlide + '"]').addClass('slick-current');
-      });
-      $('[data-slide]').on("click", function(e){
-        e.preventDefault();
-        var slideno = $(this).data('slide');
-        psliderFor.slick('slickGoTo', slideno);
-      });
+      if($(window).width() >= 1024){
+          psliderFor.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            cssEase: 'ease',
+            arrows: false,
+            dots: true,
+            appendDots: $("<div class='slick-dots-wrap'></div>"),
+          });
+          // Set initial class for the first dot
+          $('.p-slick-dot[data-slide="0"]').addClass('slick-current slick-first-slide');
+          psliderFor.on('afterChange', function (event, slick, currentSlide, nextSlide) {
+            $('.p-slick-dot').removeClass('slick-current');
+            $('.p-slick-dot[data-slide="' + currentSlide + '"]').addClass('slick-current');
+          });
+          $('[data-slide]').on("click", function(e){
+            e.preventDefault();
+            var slideno = $(this).data('slide');
+            psliderFor.slick('slickGoTo', slideno);
+          });
 
-      $('.p-slick-arrow').on("click", function(e){
-        e.preventDefault();
-        const currentSlide = psliderFor.slick('slickCurrentSlide');
-        const totalSlides = psliderFor.slick('getSlick').slideCount - 1;
-        if ($(this).hasClass('p-slick-next')) {
-          const nextSlide = currentSlide + 1;
-          if (nextSlide <= totalSlides) {
-            psliderFor.slick('slickGoTo', nextSlide);
-            if (nextSlide === totalSlides) {
-              $(this).closest('.p-slick-dot').next(".p-slick-dot").addClass('slick-last-slide');
+          $('.p-slick-arrow').on("click", function(e){
+            e.preventDefault();
+            const currentSlide = psliderFor.slick('slickCurrentSlide');
+            const totalSlides = psliderFor.slick('getSlick').slideCount - 1;
+            if ($(this).hasClass('p-slick-next')) {
+              const nextSlide = currentSlide + 1;
+              if (nextSlide <= totalSlides) {
+                psliderFor.slick('slickGoTo', nextSlide);
+                if (nextSlide === totalSlides) {
+                  $(this).closest('.p-slick-dot').next(".p-slick-dot").addClass('slick-last-slide');
+                }
+              }
+            } else if ($(this).hasClass('p-slick-prev')) {
+              const prevSlide = currentSlide - 1;
+              if (prevSlide >= 0) {
+                psliderFor.slick('slickGoTo', prevSlide);
+                if (prevSlide === 0) {
+                  $(this).closest('.p-slick-dot').prev(".p-slick-dot").addClass('slick-first-slide');
+                }
+              }
             }
-          }
-        } else if ($(this).hasClass('p-slick-prev')) {
-          const prevSlide = currentSlide - 1;
-          if (prevSlide >= 0) {
-            psliderFor.slick('slickGoTo', prevSlide);
-            if (prevSlide === 0) {
-              $(this).closest('.p-slick-dot').prev(".p-slick-dot").addClass('slick-first-slide');
+          });
+      }
+      else{
+        psliderFor.slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: true,
+          cssEase: 'ease',
+          arrows: false,
+          dots: false,
+          swipeToSlide: true,
+          focusOnSelect: true,
+          asNavFor: psliderNav
+        });
+        psliderNav.slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 1500,
+          swipeToSlide: true,
+          focusOnSelect: true,
+          dots: false,
+          arrows: true,
+          prevArrow: '<div class="slick-arrow slick-prev button btn-green flex flex-center"><span class="fa-regular fa-arrow-left"></span></div>',
+          nextArrow: '<div class="slick-arrow slick-next button btn-green flex flex-center"><span class="fa-regular fa-arrow-right"></span></div>',
+          asNavFor: psliderFor,
+          responsive: [
+            {
+            breakpoint: 739,
+                settings: {
+                  dots: true,
+                }
             }
-          }
-        }
-      });
+          ]
+        });  
+      }
+      
 });
