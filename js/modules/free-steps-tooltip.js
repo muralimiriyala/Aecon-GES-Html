a59f05220a13b3f7c6f9bc7177f070d524edf2ee
MuraliJbk <murali@jbkinfotech.com>
2024-01-12 21:22:01 +0530

    deve.

--- js/modules/free-steps-tooltip.js
+++ js/modules/free-steps-tooltip.js
@@ -3,16 +3,24 @@
 jQuery(function($){
     const _selffImage = $(".free-steps-thumb");
     const _tooltip = $(".tooltip-main");
-    const _selfOffset = _selffImage.offset();
     _selffImage.on("mouseenter mousemove", function(e){
         _tooltip.fadeIn();
+        _tooltipHeight = _tooltip.outerHeight(true);
+        _tooltipWidth = _tooltip.width();
+
+        let _selfOffset = _selffImage.offset();
         let mouseY = e.pageY - _selfOffset.top;
         let mouseX = e.pageX - _selfOffset.left;
         _tooltip.css({
-            "top": mouseY,
-            "left": mouseX,
+            "top": mouseY -  180,
+            "left": mouseX - 104,
         })
-    }).on("mouseleave", function(){
+    })
+    _selffImage.on("mouseleave", function(){
         _tooltip.fadeOut();
     })
+    _tooltip.on("mouseenter", function(){
+        _tooltip.stop(true, true).show();
+    });
+
 });
