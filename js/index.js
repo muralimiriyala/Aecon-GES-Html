95ac1aaa576d08f420f87c9ddf736bb4ae9f6cbc
MuraliJbk <murali@jbkinfotech.com>
2024-01-15 10:16:37 +0530

    deve.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 "use strict";
 
-import './modules/custom-slick';
+import './modules/animation';
 import './modules/custom-script';
 import './modules/browse-mobile';
 import './modules/explore-slider';
 import './modules/partners-tab';
 import './modules/free-steps-tooltip';
+import './modules/custom-slick';
 import './lib/jquery.selectBox';
 import './modules/custom-selectBox';
