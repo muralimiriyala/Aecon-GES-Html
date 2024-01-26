f6df7ad0055729e9c76e2c9dec9914798ae5349a
MuraliJbk <murali@jbkinfotech.com>
2024-01-26 20:46:48 +0530

    deve.

--- js/modules/our-approach-link.js
+++ js/modules/our-approach-link.js
@@ -1,51 +1,45 @@
-
-
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
   var _windowWidth = $(window).width();
-  $("ul.our-approach-links li:first-child").addClass("open-approach");
-    const approachLinks = $(".our-approach-link");
-    approachLinks.on("click", function(e){
-        e.preventDefault();
-        const _self = $(this);
-        _self.parent().siblings("li").find("a").removeClass("active");
-        _self.addClass("active");
-         
-        // Find the corresponding slick slider index
-        var index = $('.our-approach-link').index(this);
-        console.log(index)
+  $('ul.our-approach-links li:first-child').addClass('open-approach');
+  const approachLinks = $('.our-approach-link');
 
-        // apply index to slide
-        $('.approach-slider-for').slick('slickGoTo', index);
-    
-        _self.parent().siblings().removeClass("open-approach");
-        _self.parent().siblings().find(".our-approach-mobile").slideUp(900);
-        _self.siblings(".our-approach-mobile").slideToggle(900);
-    });
+  approachLinks.on('click', function (e) {
+    e.preventDefault();
+    const _self = $(this);
+    _self.parent().siblings('li').find('a').removeClass('active');
+    _self.addClass('active');
 
-    $('.approach-slider-for').slick({
-        slidesToScroll: 1,
+    const section = _self.closest('.our-approach-main'); 
+    var index = $('.our-approach-link').index(this);
+    section.find('.approach-slider-for').slick('slickGoTo', index);
+
+    _self.parent().siblings().removeClass('open-approach');
+    _self.parent().siblings().find('.our-approach-mobile').slideUp(900);
+    _self.siblings('.our-approach-mobile').slideToggle(900);
+  });
+
+  $('.approach-slider-for').slick({
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    arrows: false,
+    fade: true,
+    asNavFor: '.approach-slider-nav',
+  });
+
+  function approachNacSlider() {
+    if (_windowWidth >= 1024) {
+      $('.approach-slider-nav').slick({
+        slidesToShow: 1,
         adaptiveHeight: true,
-        arrows: false,
         fade: true,
-        asNavFor: '.approach-slider-nav',
+        arrows: false,
+        asNavFor: '.approach-slider-for',
       });
-      
-      function introSlider() {
-          if (_windowWidth >= 1024) {
-          $('.approach-slider-nav').slick({
-          slidesToShow: 1,
-          adaptiveHeight: true,
-          fade: true,
-          arrows: false,
-          asNavFor: '.approach-slider-for',
-        });
-      }
-      else{
-        if ($('.approach-slider-nav').hasClass('slick-initialized')) {
-          $('.approach-slider-nav').slick('unslick');
-        }
+    } else {
+      if ($('.approach-slider-nav').hasClass('slick-initialized')) {
+        $('.approach-slider-nav').slick('unslick');
       }
     }
-    introSlider();
-
-    });
+  }
+  approachNacSlider();
+});
