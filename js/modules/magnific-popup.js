d15f69880a05633cbbbae5f92bca0dc0e6624a0f
MuraliJbk <murali@jbkinfotech.com>
2024-01-27 22:51:19 +0530

    deve.

--- js/modules/magnific-popup.js
+++ js/modules/magnific-popup.js
@@ -5,7 +5,16 @@ jQuery(document).ready(function($){
     mainClass: 'mfp-video',
     removalDelay: 160,
     preloader: false,
-    fixedContentPos: true
+    fixedContentPos: true,
+    iframe: {
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+          src: 'https://www.youtube.com/embed/%id%?autoplay=1&mute=1'
+        }
+      }
+    }
   });
   $('.popup-video').magnificPopup({
     type: 'iframe',
