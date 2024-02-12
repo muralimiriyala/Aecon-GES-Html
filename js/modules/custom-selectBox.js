297d24b0ab1b6aaa6e336ed3d44916015efc9a3a
MuraliJbk <murali@jbkinfotech.com>
2024-02-12 12:08:59 +0530

    test

--- js/modules/custom-selectBox.js
+++ js/modules/custom-selectBox.js
@@ -6,4 +6,8 @@ jQuery(document).ready(function($){
         hideOnWindowScroll: true,
     });
     $(".selectBox, .selectBox-dropdown .selectBox-label").removeAttr('style');
+    // $('select').selectBox().each(function(){
+    //     const _index = $(this).index();
+    //     $('ul.selectBox-dropdown-menu').eq(0).addClass(`select-menu-${0}`);
+    // });
 });
