87a1227dca7b7a29b83adeadbc80263818b1a351
MuraliJbk <murali@jbkinfotech.com>
2024-02-19 19:31:12 +0530

    test

--- js/modules/custom-selectBox.js
+++ js/modules/custom-selectBox.js
@@ -11,3 +11,22 @@ jQuery(document).ready(function($){
     //     $('ul.selectBox-dropdown-menu').eq(0).addClass(`select-menu-${0}`);
     // });
 });
+
+
+jQuery(function($){
+    function hidemySelectBox() {
+       $(".mfp-wrap").on("scroll", function(){
+            var selectBox = new SelectBox($('select'), settings = {});
+            selectBox.hideMenus();
+       });        
+    }
+    hidemySelectBox();
+    const observer = new MutationObserver(function (mutationsList) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                hidemySelectBox();
+            }
+        }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+});
